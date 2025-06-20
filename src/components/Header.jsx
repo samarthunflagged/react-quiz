9cac2c30bc12441e0d9b01e65cc08b42115b0bd7
Samarth Patel <samarthnimeshkumarpatel@gmail.com>
2025-06-20 13:07:17 +0530

    modified:   index.html 	renamed:    src/assets/react.svg -> public/react.svg 	modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className='app-header'>
-      <img src='src\assets\react.svg' alt='React logo' />
+      <img src='/react.svg' alt='React logo' />
       <h1>The React Quiz</h1>
     </header>
   );
